10a94e2b14ab4bc94d54eb0319c6211b3ade6998
CrBarth94 <cristianbarth1@gmail.com>
2023-10-08 20:54:14 -0300

    local

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,7 +4,6 @@ const getCountryById = require('../controllers/getCountryById');
 const getCountryByName = require('../controllers/getCountryByName');
 const postActivity = require('../controllers/postActivitie');
 const getActivities = require('../controllers/getActivities');
-const { deletActivitie } = require('../../../client/src/redux/actions');
 const router = Router();
 
 router.get('/countries/name', getCountryByName);
@@ -16,7 +15,5 @@ router.get('/countries/:id', getCountryById);
 router.post('/activities', postActivity);
 
 router.get('/activities', getActivities);
-
-router.delete('/activities/name', deletActivitie);
 
 module.exports = router;
